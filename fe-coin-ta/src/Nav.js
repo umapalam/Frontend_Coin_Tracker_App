a654b3669672ab597b19160485fee77b944a74ae
pqasmi <pmqas002@gmail.com>
2021-10-27 22:28:42 -0400

    sign in/out and session destroy working

--- fe-coin-ta/src/Nav.js
+++ fe-coin-ta/src/Nav.js
@@ -15,7 +15,7 @@ export default function Nav(props) {
                 <label htmlFor="name">Username: </label>
                 <input type="text" id="name" name="username"/>
                 <label htmlFor="name">Password: </label>
-                <input type="text" id="password" name="password"/>
+                <input type="password" id="password" name="password"/>
                 <input class="button-primary"type="submit" value="signup" />
             </form>
         </nav>
